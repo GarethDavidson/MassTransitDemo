bad53a8436d9863386357d5093ddfd8f53b00330
GarethDavidson <gareth.davidson@carfinance.co.uk>
2017-03-10 17:37:01 +0000

    WIP

---
+++ QueueMessage/Program.cs
@@ -0,0 +1,40 @@
+﻿using MassTransit;
+using Messaging.Models.Interfaces;
+using Messaging.Models;
+using System;
+
+namespace TestSubscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(x =>
+              {
+                  var host = x.Host(new Uri("rabbitmq://localhost/"), h =>
+                   {
+                       h.Username("guest");
+                       h.Password("guest");
+                   });
+                
+                  x.ReceiveEndpoint(host, "MtPubSubExample_TestSubscriber", ep =>
+                   {
+                       ep.Consumer<ScheduleNotificationConsumer>();
+                   });
+
+                  x.ReceiveEndpoint(host, "message_log", e =>
+                  {
+                      e.Consumer(() => new MessageLogConsumer(new SubmitMessage<HelloWorld>("http://localhost:56045", "api/Hello")));
+                  });
+              });
+
+            bus.Start();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            bus.Stop();
+           
+        }
+    }
+}
---
+++ TestSubscriber/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestSubscriber")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestSubscriber")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("58288be7-6f01-4556-b6f6-ef25b662c685")]
