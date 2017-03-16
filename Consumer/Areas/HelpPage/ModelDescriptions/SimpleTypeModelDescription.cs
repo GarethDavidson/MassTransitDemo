04aaf9e12b734870bb1feef43901419d38f8f9cd
GarethDavidson <gareth.davidson@carfinance.co.uk>
2017-03-16 14:09:08 +0000

    Removal of conumer for now

--- Consumer/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Consumer.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- Consumer/Startup.cs
+++
@@ -1,7 +0,0 @@
-﻿
-namespace Consumer
-{
-    public partial class Startup
-    {
-    }
-}
