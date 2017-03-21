1f4b83d09df77104ee6c3ec4dc30328513412ee8
GarethDavidson <gareth.davidson@carfinance.co.uk>
2017-03-21 11:02:46 +0000

    file removal

--- Messaging/Models/HelloWorld.cs
+++
@@ -1,9 +0,0 @@
-﻿using Messaging.Models.Interfaces;
-
-namespace Messaging.Models
-{
-    public class HelloWorld : IHelloWorld
-    {
-        public string MessageSend { get; set; }
-    }
-}
--- QueueMessage/ISubmitMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace TestSubscriber
-{
-    public interface ISubmitMessage<T>
-    {
-        Task Submit(T message);
-    }
-}
